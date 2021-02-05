e654997130e1d5984ba3747dbd46724681a00559
Phodal Huang <h@phodal.com>
2021-02-05 21:47:19 +0800

    cases: add nginx examples

---
+++ nginx/public/js/index.js
@@ -0,0 +1,40 @@
+d3.json("data/cloc.json").then(function (json) {
+  var data;
+  var maxlen = 0;
+  for (let i = 0; i < json.length; i++) {
+    if (json[i].reports.length > maxlen) {
+      maxlen = json[i].reports.length;
+      data = json[i];
+    }
+  }
+
+  renderPacking(data["reports"])
+  renderNestedTreemap(data["reports"])
+});
+
+d3.json("data/git.json").then(function (json) {
+  let data = [];
+  for (let datum of json) {
+    data.push({
+      name: datum.name,
+      author: datum.author,
+      start: datum.first_commit_date,
+      end: datum.last_commit_date,
+      format_start: datum.first_commit_str,
+      format_end: datum.last_commit_str,
+      commits: datum.commits,
+    })
+  }
+
+  renderBranches(data)
+});
+
+d3.json("data/git-commits.json").then(function (data) {
+  renderCommitsTree(data);
+
+  renderMembersTimeline(commit_to_author_map(data));
+
+  renderHeatmapChart("#hour-heatmap", commit_to_hour_date(data));
+  renderHeatmapChart("#hour-heatmap-half-year", commit_to_hour_date(data, {before_month: 6}));
+  renderHeatmapChart("#hour-heatmap-three-month", commit_to_hour_date(data, {before_month: 3}));
+});
